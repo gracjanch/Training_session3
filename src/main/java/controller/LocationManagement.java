2e824821294787fee7f4c275ef7a33c1533d070a
igor <kolodziejigor@gmail.com>
2022-02-28 14:37:23 +0100

    abstract CsvReader class

--- src/main/java/controller/LocationManagement.java
+++ src/main/java/controller/LocationManagement.java
@@ -11,7 +11,7 @@ public class LocationManagement {
     }
 
     public List<Location> getListOfAllLocations(){
-        CsvReader csvReader = new CsvReader();
+        CsvReader csvReader = new CsvLocationLoader();
         return csvReader.read();
     }
 
