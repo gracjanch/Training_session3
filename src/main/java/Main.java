3f0722118536fac210b4a05e73866f1f42badeef
igor <kolodziejigor@gmail.com>
2022-02-28 12:18:49 +0100

    file reader and writer added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,23 @@
+import location.Location;
+import reader.CsvReader;
 import userService.UserManagement;
+import writer.CsvWriter;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserManagement userManagement = new UserManagement();
-        userManagement.start();
+//        UserManagement userManagement = new UserManagement();
+//        userManagement.start();
+        CsvWriter csvWriter = new CsvWriter();
+        Location location = new Location("50x100", "city", "region", "country");
+        csvWriter.write(location);
+
+        CsvReader csvReader = new CsvReader();
+        List<Location> l = csvReader.read();
+
+        l.stream().forEach(System.out::println);
+
+
     }
 }
