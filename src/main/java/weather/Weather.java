360e2d487e40dc1c64f83a486469d17e0642ddad
gracho <gracho1221@wp.pl>
2022-02-28 11:04:39 +0100

    weather modification and location added

--- src/main/java/weather/Weather.java
+++ src/main/java/weather/Weather.java
@@ -1,4 +1,57 @@
 package weather;
 
 public class Weather {
+    private String temperature;
+    private String pressure;
+    private String humidity;
+    private String wind;
+
+    public Weather(String temperature, String pressure, String humidity, String wind) {
+        this.temperature = temperature;
+        this.pressure = pressure;
+        this.humidity = humidity;
+        this.wind = wind;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(String pressure) {
+        this.pressure = pressure;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getWind() {
+        return wind;
+    }
+
+    public void setWind(String wind) {
+        this.wind = wind;
+    }
+
+    @Override
+    public String toString() {
+        return "Weather{" +
+                "temperature='" + temperature + '\'' +
+                ", pressure='" + pressure + '\'' +
+                ", humidity='" + humidity + '\'' +
+                ", wind='" + wind + '\'' +
+                '}';
+    }
 }
