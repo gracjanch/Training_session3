d4183ab4ef0ab525f55c978ddee536d4d610cb8f
gracho <gracho1221@wp.pl>
2022-03-01 15:15:49 +0100

    update LocationValidate and UserValidate

--- src/main/java/controller/LocationValidate.java
+++ src/main/java/controller/LocationValidate.java
@@ -7,20 +7,50 @@ import java.util.regex.Pattern;
 public class LocationValidate {
 
     public static String validCity(){
-        boolean x;
+        boolean properlyValue;
         String cityLocation;
         do{
             cityLocation = new Scanner(System.in).nextLine();
             Pattern nameCityPattern = Pattern.compile("[a-z]{2}+");
             Matcher m = nameCityPattern.matcher(cityLocation);
-            x = m.find();
-            if(x == true){
-                return cityLocation;
+            properlyValue = m.find();
+            if(properlyValue){
+               break;
             }
             System.out.println("Incorrect data. Write city name again.");
-        }while(x != true);
+        }while(!properlyValue);
         return cityLocation;
     }
+
+    public static String validCoordinate() {
+        boolean properlyValue;
+        String cityCoordinate;
+        boolean firstCoordBoolean = false;
+        boolean secondCoordBoolean = false;
+        do {
+            cityCoordinate = new Scanner(System.in).nextLine();
+            Pattern nameCityPattern = Pattern.compile("[-]*+[0-9]+[x]+[-]*[0-9]+");
+            Matcher m = nameCityPattern.matcher(cityCoordinate);
+            properlyValue = m.find();
+            if(!properlyValue){
+                System.out.println("Incorrect data. Write coordinates again.");
+                continue;
+            }
+
+            String[] coordinates = cityCoordinate.split("x");
+            Integer firstCoord = Integer.valueOf(coordinates[0]);
+            Integer secondCoord = Integer.valueOf(coordinates[1]);
+            firstCoordBoolean = firstCoord >= -90 && firstCoord <= 90;
+            secondCoordBoolean = secondCoord >= -180 && secondCoord <= 180;
+
+            if (firstCoordBoolean && secondCoordBoolean) {
+                break;
+            }
+            System.out.println("Incorrect data. Write coordinates again.");
+        } while (!properlyValue && !firstCoordBoolean && !secondCoordBoolean);
+        return cityCoordinate;
+    }
+
 
 
 
