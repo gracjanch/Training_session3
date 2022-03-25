78f194a727f7f59df7118a1b6710bc8cbfb6deec
gracho <gracho1221@wp.pl>
2022-03-25 17:49:07 +0100

    adding CountryDao and LocationDao test

---
+++ src/test/java/dao/LocationDaoTest.java
@@ -0,0 +1,75 @@
+package dao;
+
+import model.entity.Country;
+import model.entity.Location;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocationDaoTest {
+    private static final Logger logger = LogManager.getLogger(LocationDaoTest.class);
+
+    private LocationDao locationDao;
+
+    Country testCountry = new Country("PL", "Polska");
+    Location testLocation = new Location("WAR", "Warszawa", "80x80",
+            "", testCountry);
+
+
+    //addLocation
+    @Test
+    public void shouldAddLocation() {
+
+        LocationDao resultLocationDao = new LocationDao();
+
+        resultLocationDao.addLocation(testLocation);
+
+        Assert.assertNotNull(resultLocationDao.getAllLocations());
+
+        resultLocationDao.deleteLocation(testLocation);
+    }
+
+
+    //findById
+    @Test
+    public void shouldFindLocationById() {
+        LocationDao resultLocationDao = new LocationDao();
+        Location locationResult = resultLocationDao.findById("KRA");
+        Assert.assertEquals(locationResult.getCity(), "Kraków");
+    }
+
+
+    //getAllLocation
+    @Test
+    public void shouldCheckGetAllLocations() {
+        LocationDao resultLocationDao = new LocationDao();
+        List<Location> listResult = resultLocationDao.getAllLocations();
+        Assert.assertNotNull(listResult);
+    }
+
+
+    //deleteLocation
+    @Test
+    public void shouldDeleteLocation() {
+
+        LocationDao resultLocationDao = new LocationDao();
+
+        resultLocationDao.addLocation(testLocation);
+
+        Location result = resultLocationDao.findById("WAR");
+
+        int sizeBeforeDelete = resultLocationDao.getAllLocations().size();
+
+        resultLocationDao.deleteLocation(result);
+
+        int sizeAfterDelete = resultLocationDao.getAllLocations().size();
+
+        Assert.assertTrue(sizeBeforeDelete == sizeAfterDelete + 1);
+    }
+
+}
